a5b9ca5f98b5c2b72e06b287de06e92d058df159
Charlie <cnewlinds@gmail.com>
2023-08-20 22:57:01 +1000

    turnery function

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+  /*   let superbowlWin = record.find(function superbowlWin(record){
+        if (record.result === "W") {
+            return record.year;
+        } else {
+            return undefined;
+        }
+    })
+
+    // const superbowlWin = (array.resilt ==="W") => array.year
+    result.find(superbowlWin);
+ */
+
+/* 
+const superbowlWin = record.find((record.result === "W") => record.year);       */
+
+
+function superbowlWin(record) {
+    let winYear = record.find(entry => entry.result === "W");
+    return winYear ? winYear.year : undefined;
+
+}
